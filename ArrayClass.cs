ca41f811fdc3e9a224229edacc0a2b9e0e6bbfcd
davidbabayan <30317936+davidbabayan@users.noreply.github.com>
2018-08-13 18:23:24 +0400

    Add files via upload

---
+++ ArrayClass.cs
@@ -0,0 +1,11 @@
+namespace ArrayManipulations
+{
+    public class ArrayClass<T>
+    {
+        public T[] arr;
+        public ArrayClass(int lenght)
+        {
+            arr = new T[lenght];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArrayManipulations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayClass<int> a = new ArrayClass<int>(3);
+            /*
+                Your code...
+            */
+            a.arr[15] = 23;
+            Console.WriteLine(a.arr[15]);
+        }
+    }
+}
